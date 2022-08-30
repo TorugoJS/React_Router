525acab0762014895809390418ef654a7303bbaa
TorugoJS <victor.devfrontend@gmail.com>
2022-08-30 19:55:05 -0300

    react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+// browswRouter => define onde a area do nosso app que vai trocar as páginas
+// Routes => define as rotas
+// Route => um elemento deste para cada rota, configurar com path e componente da rota
+
+// 1 - config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
