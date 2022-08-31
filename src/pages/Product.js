152c4a35ef78029441f18cdc3468795549fdaa23
TorugoJS <victor.devfrontend@gmail.com>
2022-08-30 21:26:03 -0300

    React-Router

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,13 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
 // 4 - rota dinamina
 
 const { id } = useParams()
 
+// 5 - carregamento individual
+
+const url = "http://localhost:3000/products/" + id
+
+const {data: product, loading, error} = useFetch(url)
+
+console.log(product)
+
   return (
     <>
     <p>ID do produto: {id}</p>
+    {error && <p>Ocorreu um erro...</p>}
+    {loading && <p>Carregando...</p>}
+    {product && (
+        <div>
+            <h1>{product.name}</h1>
+            <p>R${product.price}</p>
+        </div>
+    )}
     </>
   )
 }
